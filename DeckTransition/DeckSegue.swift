e258475a2ecf08ab9e4f6bed2ff3383857e39707
Finn Gaida <f@fga.pw>
2018-12-13 10:57:49 +0100

    update deck segue for completion instead of animation

--- DeckTransition/DeckSegue.swift
+++ DeckTransition/DeckSegue.swift
@@ -11,11 +11,11 @@ import UIKit
 /// A segue to implement the deck transition via Storyboards
 public final class DeckSegue: UIStoryboardSegue {
 
-    public var dismissAnimation: (() -> ())?
+    public var dismissCompletion: (() -> ())?
     var transition: UIViewControllerTransitioningDelegate!
 
     override public func perform() {
-        transition = DeckTransitioningDelegate(dismissAnimation: dismissAnimation)
+        transition = DeckTransitioningDelegate(dismissCompletion: dismissCompletion)
         destination.transitioningDelegate = transition
         destination.modalPresentationStyle = .custom
         source.present(destination, animated: true, completion: nil)
